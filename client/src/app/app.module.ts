37117ced00adca33161b9385da34384777ef8a1c
Guy Signer <guy.signer@webpearls.net>
2025-05-13 14:46:59 +0300

    matrial ui

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -12,6 +13,7 @@ import { TasksModule } from './features/tasks/tasks.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CoreModule,
     SharedModule,
     TasksModule
--- client/src/app/features/tasks/tasks.module.ts
+++ client/src/app/features/tasks/tasks.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// Angular Material Imports
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
@@ -15,7 +25,16 @@ import { TaskService } from './services/task.service';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    // Angular Material Modules
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatToolbarModule
   ],
   exports: [
     TaskListComponent
